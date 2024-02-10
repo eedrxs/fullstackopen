a4e4890fe50a018479bc47817fbcb4abf890999a
eedris <alabialade@gmail.com>
2024-02-10 15:55:57 +0100

    finished exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log("provide password as argument")
+  process.exit(1)
+}
+
+const [password, name, number] = process.argv.slice(2)
+const url = `mongodb+srv://eedris:${password}@cluster0.vagojos.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model("Person", personSchema)
+
+if (name && number) {
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then((persons) => {
+    console.log("phonebook:")
+    persons.forEach(({ name, number }) => console.log(`${name} ${number}`))
+    mongoose.connection.close()
+  })
+}

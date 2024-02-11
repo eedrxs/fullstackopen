b2d75b7820170b9dc12e317f41f445c8fc029545
eedris <alabialade@gmail.com>
2024-02-11 15:25:11 +0100

    returned phonebook app in part2 folder to how it was at the end of exercise 2.20

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,7 +4,7 @@ import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 import Notification from "./components/Notification"
 
-import personService from "./services/person"
+import contactService from "./services/contacts"
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -14,25 +14,25 @@ const App = () => {
   const [notification, setNotification] = useState(null)
 
   useEffect(() => {
-    personService.getAll().then((persons) => setPersons(persons))
+    contactService.getAll().then((contacts) => setPersons(contacts))
   }, [])
 
-  const addPerson = (e) => {
+  const addContact = (e) => {
     e.preventDefault()
     const newPerson = { name: newName, number: newNumber }
-    const existingPerson = persons.find(({ name }) => name === newName)
-
-    if (existingPerson) {
+    const existingContact = persons.find(({ name }) => name === newName)
+    
+    if (existingContact) {
       const wantsToEdit = confirm(
         `${newName} is already added to phonebook, replace the old number with a new one?`
       )
       if (wantsToEdit) {
-        personService
-          .update(existingPerson.id, newPerson)
+        contactService
+          .update(existingContact.id, newPerson)
           .then(() => {
             showNotification(`Updated ${newName}`, "success")
-            personService.getAll().then((persons) => {
-              setPersons(persons)
+            contactService.getAll().then((contacts) => {
+              setPersons(contacts)
               clearInputs()
             })
           })
@@ -41,30 +41,23 @@ const App = () => {
               showNotification(
                 `Information of ${newName} has already been removed from server`
               )
-            } else if (err.response.status === 400) {
-              showNotification(err.response.data.error)
             }
           })
       }
     } else {
-      personService
-        .create(newPerson)
-        .then((newPerson) => {
-          showNotification(`Added ${newName}`, "success")
-          setPersons([...persons, newPerson])
-          clearInputs()
-        })
-        .catch((err) => {
-          showNotification(err.response.data.error)
-        })
+      contactService.create(newPerson).then((newContact) => {
+        showNotification(`Added ${newName}`, "success")
+        setPersons([...persons, newContact])
+        clearInputs()
+      })
     }
   }
 
-  const deletePerson = (id, name) => {
+  const deleteContact = (id, name) => {
     if (confirm(`Delete ${name} ?`)) {
-      personService.deleteContact(id).then(() => {
+      contactService.deleteContact(id).then(() => {
         showNotification(`Deleted ${name}`, "success")
-        personService.getAll().then((persons) => setPersons(persons))
+        contactService.getAll().then((contacts) => setPersons(contacts))
       })
     }
   }
@@ -98,11 +91,11 @@ const App = () => {
         newNumber={newNumber}
         onNewName={setNewName}
         onNewNumber={setNewNumber}
-        onAddPerson={addPerson}
+        onAddPerson={addContact}
       />
 
       <h3>Numbers</h3>
-      <Persons persons={filteredPersons} onDeleteContact={deletePerson} />
+      <Persons persons={filteredPersons} onDeleteContact={deleteContact} />
     </div>
   )
 }

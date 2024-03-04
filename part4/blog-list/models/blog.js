979e360a411b5798ccaa5b536982894799875b01
eedris <alabialade@gmail.com>
2024-03-04 11:44:40 +0100

    finished exercises 4.3 - 4.7

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -7,12 +7,4 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-blogSchema.set('toJSON', {
-  transform: (doc, obj) => {
-    obj.id = obj._id.toString()
-    delete obj._id
-    delete obj.__v
-  }
-})
-
 module.exports = mongoose.model('Blog', blogSchema)

ace55cb8be18e6088330013d3ebe1f5d633c95c6
eedris <alabialade@gmail.com>
2024-03-10 16:06:12 +0100

    quick edit

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -18,7 +18,8 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-app.use(cors());                                app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/blogs', blogsRouter)
 
